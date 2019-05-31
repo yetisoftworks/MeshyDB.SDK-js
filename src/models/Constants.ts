fa729ca8d0f57b99ad22aab268c4af2d8fc631e9
Chris Pluta <chris.pluta@yetisoftworks.com>
2019-05-31 14:04:12 -0500

    Adding tests for constants

--- src/models/Constants.ts
+++ src/models/Constants.ts
@@ -4,6 +4,14 @@ export class Constants {
   public publicKey: string = '';
   public tenant: string | null | undefined = null;
   constructor(clientKey: string, publicKey: string, tenant?: string) {
+    if (!clientKey) {
+      throw new Error("Missing parameter: clientKey");
+    }
+    
+    if (!publicKey) {
+      throw new Error("Missing parameter: publicKey");
+    }
+
     this.apiUrl = `https://api.meshydb.com/${clientKey}`;
     this.authUrl = `https://auth.meshydb.com/${clientKey}`;
     this.publicKey = publicKey;
--- tests/models/Constants.test.ts
+++ tests/models/Constants.test.ts
@@ -4,3 +4,48 @@ import { v4 as guid } from 'uuid';
 test("Should create constants", () => {
     expect(new Constants(guid(), guid())).toBeTruthy();
 });
+
+test("Should create constants with tenant", () => {
+    expect(new Constants(guid(), guid(), guid())).toBeTruthy();
+});
+
+test("Constants should throw error when client key is empty", () => {
+    expect(() => { new Constants("", guid(), guid()) }).toThrowError();
+});
+
+test("Constants should throw error when public key is empty", () => {
+    expect(() => { new Constants(guid(), "", guid()) }).toThrowError();
+});
+
+test("Api url should be configured", () => {
+    const clientKey = guid();
+    const constants = new Constants(clientKey, guid(), guid());
+
+    expect(constants.apiUrl).toBe(`https://api.meshydb.com/${clientKey}`);
+});
+
+test("Auth url should be configured", () => {
+    const clientKey = guid();
+    const constants = new Constants(clientKey, guid(), guid());
+
+    expect(constants.authUrl).toBe(`https://auth.meshydb.com/${clientKey}`);
+});
+
+test("Should create constants with tenant", () => {
+    const tenant = guid();
+    const constants = new Constants(guid(), guid(), tenant);
+    expect(constants.tenant).toBe(tenant);
+});
+
+
+test("Public key should be copied", () => {
+    const publicKey = guid();
+    const constants = new Constants(guid(), publicKey, guid());
+    expect(constants.publicKey).toBe(publicKey);
+});
+
+test("Tenant should be copied", () => {
+    const tenant = guid();
+    const constants = new Constants(guid(), guid(), tenant);
+    expect(constants.tenant).toBe(tenant);
+});
